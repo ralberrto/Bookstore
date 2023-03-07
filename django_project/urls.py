0fa4212b352308b2cd566334c031a087310c4483
Alberto Larios <albertolarios@me.com>
2023-03-07 09:37:59 -0600

    Add login, logout and signup functionality to project

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    # User managment
+    path("users/", include("django.contrib.auth.urls")),
+    path("users/", include("users.urls")),
+    # Local apps
     path("", include("pages.urls")),
 ]
