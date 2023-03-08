93da2d1d2bb02b2a26f1d20fd2e8685ef7a0d20e
Alberto Larios <albertolarios@me.com>
2023-03-08 10:40:24 -0600

    Modify tests for new signup page using django_allauth

--- users/tests.py
+++ users/tests.py
@@ -2,9 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from .views import SignupPageView
-from .forms import CustomUserCreationForm
-
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -36,22 +33,21 @@ class CustomUserTests(TestCase):
 
 class SignupPageTests(TestCase):
     def setUp(self):
-        url = reverse("signup")
+        url = reverse("account_signup")
         self.response = self.client.get(url)
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertTemplateUsed(self.response, "account/signup.html")
         self.assertContains(self.response, "Sign Up")
         self.assertNotContains(
             self.response, "Hi there! I probably shouldn't be on the page!"
         )
 
     def test_signup_form(self):
-        form = self.response.context.get("form")
-        self.assertIsInstance(form, CustomUserCreationForm)
-        self.assertContains(self.response, "csrfmiddlewaretoken")
-
-    def test_signup_view(self):
-        view = resolve("/users/signup/")
-        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
+        new_user = get_user_model().objects.create_user(
+            username="test", email="test@email.com", password="some234!sdF"
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "test")
+        self.assertEqual(get_user_model().objects.all()[0].email, "test@email.com")
