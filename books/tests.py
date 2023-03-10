bf85b821754ef189d24db68b37dad05322d33fb4
Alberto Larios <albertolarios@me.com>
2023-03-09 21:54:26 -0600

    Add test for book views and book model

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Los Bandidos de Río Frío",
+            author="Manuel Payno",
+            price="419",
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(f"{self.book.title}", "Los Bandidos de Río Frío")
+        self.assertEqual(f"{self.book.author}", "Manuel Payno")
+        self.assertEqual(f"{self.book.price}", "419")
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Los Bandidos")
+        self.assertTemplateUsed(response, "books/book_list.html")
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get("/books/1234")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Los Bandidos")
+        self.assertTemplateUsed(response, "books/book_detail.html")
