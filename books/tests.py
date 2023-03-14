6b21544ac471443c5273c7033d3cddd8c2dfe2cb
Alberto Larios <albertolarios@me.com>
2023-03-13 20:12:11 -0600

    Add test for Review in Book detail page

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,29 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="aoji234WEF239$%",
+        )
+
         cls.book = Book.objects.create(
             title="Los Bandidos de Río Frío",
             author="Manuel Payno",
             price="419",
+        )
+
+        cls.review = Review.objects.create(
+            book=cls.book,
+            author=cls.user,
+            review="An excellent review",
         )
 
     def test_book_listing(self):
@@ -30,4 +43,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Los Bandidos")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
